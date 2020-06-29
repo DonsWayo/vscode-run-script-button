22a6032594e627392e24c377eb0960664f93575a
juan jose carracedo <jjcv-10@hotmail.com>
2020-06-29 23:51:21 +0200

    added icon, title bar conditions, refactor and support npm & yarn

--- src/select-script.ts
+++ src/select-script.ts
@@ -1,17 +1,18 @@
 import { window, workspace, Uri } from 'vscode';
-
+import { existsSync } from 'fs';
 
 export async function showScripts() {
-    let wok = workspace.rootPath;
+    const wok = workspace.rootPath;
 
     if (wok) {
-        let packageJsonPath = (`${wok}/package.json`);
-        let readPakageJson = await workspace.fs.readFile(Uri.file(packageJsonPath));
-        let jsonOutput = JSON.parse(readPakageJson.toString());
+        const packageJsonPath = (`${wok}/package.json`);
+        const checkPkgManager = existsSync(`${wok}/package-lock.json`);
+        const useNpm = checkPkgManager ? true : false;
+        const runCommand = useNpm ? 'npm' : 'yarn';
+        const readPakageJson = await workspace.fs.readFile(Uri.file(packageJsonPath));
+        const jsonOutput = JSON.parse(readPakageJson.toString());
         window.showQuickPick(Object.keys(jsonOutput.scripts)).then(async response => {
             if (response) {
-                window.showInformationMessage(response);
-                //const terminal = window.createTerminal(`${response}`, '/bin/bash', 'npm run lint');
                 const terminal = window.createTerminal({
                     cwd: wok,
                     hideFromUser: false,
@@ -19,10 +20,12 @@ export async function showScripts() {
                 });
                 terminal.show();
                 setTimeout(() => {
-                    terminal.sendText(`npm run ${response}`);
+                    terminal.sendText(`${runCommand} run ${response}`);
                 }, 1000);
             }
         });
+    } else {
+        window.showErrorMessage('Workspace not found');
     }
 
 }
